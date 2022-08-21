d1aa4d209b167ce68881228fdc1b2204a33936a9
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2022-08-21 16:31:52 -0400

    Basic code to download, process, upload, post message

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This is a proof-of-concept for the COMPASS automatic data-processing
 setup requested by SCP:
 
-* Long-lived tokens for Dropbox and Google Drive
+* Long-lived tokens for Dropbox, Google Drive, and Slack
 * A shell script to load R and any other needed tools
 * An R script to download data from Dropbox, process, and upload to Google Drive
 * Error reporting
--- autopipe.R
+++ autopipe.R
@@ -1,2 +1,42 @@
 # autopipe.R
 
+
+
+# Dropbox download --------------------------------------------------------
+
+library(rdrop2)
+refreshable_token <- readRDS("refreshable_droptoken.rds")
+info <- drop_acc(refreshable_token)
+message("Dropbox account: ", info$name$display_name)
+
+library(compasstools)
+DROPBOX_DIR <- "TEMPEST_PNNL_Data/Current_data"
+sf_raw <- compasstools::process_sapflow_dir(DROPBOX_DIR,
+                                            tz = "EST",
+                                            dropbox_token = refreshable_token)
+test_n_obs <- nrow(sf_raw)
+write.csv(head(sf_raw), "data_upload/sf_raw.csv")
+
+
+# Google Drive upload -----------------------------------------------------
+
+# TODO: this will need to be a refreshable token?
+
+# x <- gargle::token_fetch()
+# saveRDS(x, "googletoken.rds")
+google_token <- readRDS("googletoken.rds")
+drive_auth(token = google_token)
+path <- drive_find("autopipe", n_max=100, type = "folder")
+drive_upload("data_upload/sf_raw.csv", path = path)
+
+
+# Post to Slack -----------------------------------------------------------
+
+# This is very basic -- I created a webhook to post to my account (#ben)
+# and saved the URL as 'slacktoken.rds'
+# Using the `slackr` function would allow for much fancier stuff
+library(slackr)
+WEBHOOK_URL <- readRDS("slacktoken.rds")
+msg <- paste("Processed", test_n_obs, "sapflow observations")
+message("Posting to Slack: ", msg)
+slackr_bot(msg, incoming_webhook_url = WEBHOOK_URL)
